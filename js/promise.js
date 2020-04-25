893f27c0c580e73fda256232cbc3eba2bbd5ed72
linzhen <linzhenc@yonyou.com>
2020-04-25 18:29:10 +0800

    +Promise A

--- js/promise.js
+++ js/promise.js
@@ -1,49 +1,80 @@
 /**
- * 模拟实现一个Promise A+规范，尚未完成
+ * 模拟实现一个Promise A规范
  * @param asyncFn
  * @returns {{then: _Promise.then, catch: _Promise.catch, status: _Promise.status}}
  * @private
  */
 const _Promise = function (asyncFn) {
-    let resolveCallbackFn = [];
-    let rejectedCallbackFn = [];
-    this.status = 'PENDING';
-
-    this.then = (fn) => {
-        resolveCallbackFn.push(fn)
-    };
-
+    this.resolveCallbackFn = [];
+    this.rejectedCallbackFn = [];
+    this.PENDING = 'PENDING';
+    this.FULFILLED = 'FULFILLED';
+    this.REJECTED = 'REJECTED';
+    this.status = this.PENDING;
+    this.data = null;
     this.catch = function (fn) {
-        rejectedCallbackFn.push(fn)
+        this.rejectedCallbackFn.push(fn)
     };
 
     let resolve = (data) => {
-        this.status = 'FULFILLED';
-        resolveCallbackFn.forEach((fn) => {
-            fn(data)
-        })
+        // debugger
+        if (this.status === this.PENDING) {
+            this.status = this.FULFILLED;
+            this.data = data;
+            this.resolveCallbackFn.forEach((fn) => {
+                fn(data)
+            })
+        }
     };
 
     let reject = (err) => {
-        this.status = 'REJECTED';
-        rejectedCallbackFn.forEach((fn) => {
-            fn(err)
-        })
+        if (this.status === this.PENDING) {
+            this.status = this.REJECTED;
+            this.data = err;
+            this.rejectedCallbackFn.forEach((fn) => {
+                fn(err)
+            })
+        }
     };
 
     asyncFn(resolve, reject);
-
-    return {then: this.then, catch: this.catch, status: this.status}
 };
 
-var p = new _Promise(function (rs, rj) {
+_Promise.prototype.then = function (onFulfilled, onRejected) {
+
+    onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : (d) => d;
+    onRejected = typeof onRejected === 'function' ? onRejected : (err) => {throw Error(err)};
+
+    //PENDING时是在初始化then
+    if (this.status === this.PENDING) {
+        this.resolveCallbackFn.push(onFulfilled);
+        this.rejectedCallbackFn.push(onRejected);
+    }
+
+    //FULFILLED时是在成功后调用成功的回调
+    if (this.status === this.FULFILLED) {
+        this.resolveCallbackFn.forEach((fn) => {
+            fn(this.data)
+        })
+    }
+
+    //REJECTED时是在失败后调用失败的回调
+    if (this.status === this.REJECTED) {
+        this.resolveCallbackFn.forEach((fn) => {
+            fn(this.data)
+        })
+    }
+    // return this
+};
+
+const p = new _Promise(function (rs, rj) {
     setTimeout(function () {
-        rs(123)
-    })
-});
-console.log(p)
-p.then((data) => {
-    console.log(data)
-    console.log(p)
+        rs('done');
+    }, 100)
 });
 
+p.then((data) => {
+    console.log('data:' + data)
+}, (err) => {
+    console.log('err:'+ err)
+});
---
+++ 设计模式/装饰器模式.js
@@ -0,0 +1,28 @@
+Function.prototype.before = function (beforeFn) {
+    const _self = this;
+    return function () {
+        beforeFn.apply(this, arguments);
+        return _self.apply(this, arguments);
+    }
+}
+
+Function.prototype.after = function (afterFn) {
+    const _self = this;
+    return function () {
+        const ret = _self.apply(this, arguments);
+        afterFn.apply(this, arguments);
+        return ret;
+    }
+}
+
+let func = function(){
+    console.log(123)
+}
+
+func = func.before(function() {
+    console.log('1');
+}).after(function() {
+    console.log('3');
+})
+
+func();
